e16db8514bc0bc9f7bbe9172144e4066a2b9ba7f
oyatrides <oyatrides@gmail.com>
2021-07-15 16:19:04 +0200

    nav creation + usage of react-modele instead of create-react-app

---
+++ front-end/src/components/App/index.js
@@ -0,0 +1,16 @@
+// == Import npm
+import React from 'react';
+import Nav from '/src/components/Nav';
+// == Import
+
+import './styles.scss';
+
+// == Composant
+const App = () => (
+  <div className="app">
+    <Nav />
+  </div>
+);
+
+// == Export
+export default App;
--- front-end/src/reducers/index.js
+++ front-end/src/reducers/index.js
@@ -1,13 +1,12 @@
-
 export const initialState = {
 
 };
 
 const reducer = (state = initialState, action = {}) => {
-    switch (action.type) {
-        default:
-            return state;
-    }
+  switch (action.type) {
+    default:
+      return state;
+  }
 };
 
 export default reducer;
