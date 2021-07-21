e9bb11f511433838439e78d66251562a8681fdfb
laetitiadeschamps <deschamps.laetitia33@gmail.com>
2021-07-21 19:31:48 +0200

    EasyAdmin - MangaCrudController : add manga ok

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Manga;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Assets;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -24,10 +25,15 @@ class DashboardController extends AbstractDashboardController
         return Dashboard::new()
             ->setTitle('Kasu');
     }
+    public function configureAssets(): Assets
+    {
+        return Assets::new()->addCssFile('css/admin.css');
+    }
 
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        yield MenuItem::linkToCrud('The Label', 'fas fa-list', Manga::class);
+        yield MenuItem::linkToCrud('Mangas', 'fas fa-book', Manga::class);
+       
     }
 }
