aefb63831159b013e6ac1f9548aab84fcf13364f
laetitiadeschamps <deschamps.laetitia33@gmail.com>
2021-07-23 15:13:26 +0200

    fix errors types User + Chats

--- src/Controller/Api/ChatController.php
+++ src/Controller/Api/ChatController.php
@@ -50,8 +50,9 @@ class ChatController extends AbstractController
             );
         } 
         // fetching all chats from one user
+        /** @var array $chats */
         $chats = $this->chatRepository->findAllByUser($id);
-        
+       
         //fetching the last message of each conversation
         
         foreach($chats as $chat){
