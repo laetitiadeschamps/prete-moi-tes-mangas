0ce9bfabb108d290e0bd2462bd1c4d24e7a7a930
Listeriaa <laeti.rd@gmail.com>
2021-07-23 15:04:51 +0200

    eventListener to add user datas to JWT token

---
+++ src/EventListener/AuthenticationSuccessListener.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\EventListener;
+use Lexik\Bundle\JWTAuthenticationBundle\Event\AuthenticationSuccessEvent;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Contracts\EventDispatcher\Event;
+
+class AuthenticationSuccessListener extends Event
+{
+
+
+    /**
+     * @param AuthenticationSuccessEvent $event
+     */
+    
+    
+    public function onAuthenticationSuccessResponse(AuthenticationSuccessEvent $event)
+    {
+        $data = $event->getData();
+        $user = $event->getUser();
+    
+        if (!$user instanceof UserInterface) {
+            return;
+        }
+    
+        $data['data'] = array(
+            'pseudo' => $user->getPseudo(),
+            'latitude' =>$user->getLatitude(),
+            'longitude' =>$user->getLongitude(),
+            'email'=>$user->getEmail(),
+            'id'=>$user->getId(),
+            'picture'=>$user->getPicture(),
+
+
+        );
+    
+        $event->setData($data);
+    }
+}
